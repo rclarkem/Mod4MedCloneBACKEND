7c17631fe7a8f8fcea351dbc616b8aa3ef195f37
Reina <r.clarkemitchell@gmail.com>
2019-12-30 15:50:48 -0500

    it works

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ has_many :authored_stories, foreign_key: :author_id, class_name: :Story
  validates :email, presence: true
 #  validates :email, confirmation: true
  validates :email, uniqueness: true
- validates :password_digest, length: { in: 6..20 }
+#  validates :password_digest, length: { in: 6..20 }
 
 # TODO add in has_many followings with their associations
 
