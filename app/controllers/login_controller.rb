505fd69bbc16946424f9f827527a4148436c3010
Reina <r.clarkemitchell@gmail.com>
2020-01-17 01:49:52 -0500

    merge with serializer

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,7 @@ class LoginController < ApplicationController
     def create
         @user = User.find_by_email(params[:email])
         if @user && @user.authenticate(params[:password])
-            render json: @user
+            render json: {message: "Valid Login"}
         else
             render json: {error: ["That does not match anyone"]}, status: :not_acceptable
         end
