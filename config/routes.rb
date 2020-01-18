611a5e569da92b6e7d313a274a5d19a2a895b937
Reina <r.clarkemitchell@gmail.com>
2020-01-18 02:48:36 -0500

    added custom route for user stories

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :followings, only: [:create, :destroy]
   resources :users
   post '/login', to: 'login#create'
+  post '/users/:id/stories/', to: 'stories#create'
+  # get '/users/stories/:id', to: 'stories#show'
   # get '/profile', to: 'users#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
