7f1de423ee13c367bdbec75a0558f783955fbc8c
Reina <r.clarkemitchell@gmail.com>
2020-01-18 00:28:10 -0500

    this branch works

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,16 +1,15 @@
 class StoriesController < ApplicationController
     before_action :find_story, only: [:show, :edit, :update, :destroy]
     
-    def index
-        if valid_token
-        @stories = Story.order(id: :asc)
-        render json: @stories
-        end
+    # def index
+    #     # if valid_token
+    #     @stories = Story.order(id: :asc)
+    #     render json: @stories
+    #     # end
         
-    end
+    # end
 
     def create
-        if valid_token
         @story = Story.create(story_params)
         if @story.valid?
             render json: @story, status: :created
@@ -18,18 +17,20 @@ class StoriesController < ApplicationController
             render json: {errors: @story.errors.full_messages}, status: 400
         end
     end
-    end
 
     def show
-        
+        # if valid_token
         render json: @story
-    
+        # end
     end
 
     def destroy
-        if valid_token
+        if valid_token && logged_in_user_decoded == @story.author_id
         @story.destroy
-        render json: @story
+            render json: {success: ['Your story deleted']}, status: 200
+        else
+            
+            render json: {errors: {:story_author => "Story belongs to another user", :reason => "This is not your story"}}, status: 400
         end
     end
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :avatar, :bio, :img 
   has_many :authored_stories
+  has_many :following_users
+  has_many :followed_users
   
 
 
